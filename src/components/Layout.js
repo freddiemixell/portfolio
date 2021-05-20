81dc146eb170f0ae0de5c5e9dc398f8c72281120
Freddie Mixell <fmixell@gmail.com>
2021-05-20 10:37:01 -0400

    restructure nav for bem

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,23 +4,25 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div className="section">
-          <div className="container" style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-            <Link to="/">
-              <h1 className="title"><span className="title--primary">F</span><span className="title--secondary">M</span></h1>
-            </Link>
-            <nav className="nav">
-              <ul className="menu">
-                <li className="menu__item">
-                  <Link to="/about">About</Link>
-                </li>
-                <li className="menu__item">
-                  <a href="https://github.com/freddiemixell">Github</a>
-                </li>
-                <li className="menu__item">
-                  <Link to="/contact">Contact</Link>
-                </li>
-              </ul>
-            </nav>
+          <div className="container">
+              <div className="menu">
+                <Link to="/">
+                    <h1 className="title"><span className="title--primary">F</span><span className="title--secondary">M</span></h1>
+                </Link>
+                <nav className="nav">
+                    <ul className="nav__wrapper">
+                        <li className="nav__item">
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li className="nav__item">
+                            <a href="https://github.com/freddiemixell">Github</a>
+                        </li>
+                        <li className="nav__item nav__item--primary">
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+              </div>
           </div>
         </div>
     );
